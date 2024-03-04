7133582e5e857b95826de4076e43ed56dd373ca1
David Masek <masd04@vse.cz>
2024-03-05 00:39:13 +0100

    a10

---
+++ minimal_selenium.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+
+options = Options()
+options.headless = True
+
+driver = webdriver.Chrome(options=options)
+driver.get("http://www.example.com")
+print(driver.title)
+driver.quit()
