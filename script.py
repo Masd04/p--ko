964c30a0305db864c2ce2ff5b0b60f6e114d484d
David Masek <masd04@vse.cz>
2024-03-04 23:41:43 +0100

    asdewfdewfdas

--- script.py
+++ script.py
@@ -1,23 +1,21 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 
-# Specify the path to ChromeDriver
-chrome_driver_path = './chromedriver-win64/chromedriver.exe'
-
-# Set up Chrome options (optional)
+# Set up Chrome options
 options = webdriver.ChromeOptions()
-options.add_argument('--headless') # Uncomment to run Chrome in headless mode
+options.add_argument('--headless')  # Run in headless mode
 options.add_argument('--disable-dev-shm-usage')
+options.add_argument('--no-sandbox')  # Bypass OS security model
+options.add_argument('--disable-gpu')  # Applicable to windows os only
+options.add_argument('--disable-extensions')
 
-# Repeat the action 3 times
+# Repeat the action 2 times
 for _ in range(2):
     # Initialize the driver
-    webdriver_service = Service(chrome_driver_path)
-    driver = webdriver.Chrome(service=webdriver_service, options=options)
+    driver = webdriver.Chrome(options=options)
 
     # Open a website
     driver.get('https://bakule.vercel.app/')
@@ -29,9 +27,8 @@ for _ in range(2):
     # Click the element
     element_to_click.click()
 
-    # Keep the browser open for 5 seconds after the click
+    # Keep the browser open for 1.5 seconds after the click
     time.sleep(1.5)
-
 
     # Close the browser
     driver.quit()
