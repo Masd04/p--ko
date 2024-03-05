f2d4966ff407dd005f1e261789dac3f1f3743131
David Masek <masd04@vse.cz>
2024-03-05 01:15:47 +0100

    yes

--- minimal_selenium.py
+++ minimal_selenium.py
@@ -11,9 +11,10 @@ chrome_options = Options()
 chrome_options.headless = True  # Run in headless mode
 # Add any other necessary options here
 
+for _ in range(1):
 # Selenium 4.x uses the 'options' argument instead of 'desired_capabilities'
-s = Service('chromedriver-win64/chromedriver.exe')  # Specify path to chromedriver if not in PATH
-driver = webdriver.Remote(
+    s = Service('chromedriver-win64/chromedriver.exe')  # Specify path to chromedriver if not in PATH
+    driver = webdriver.Remote(
     command_executor='http://localhost:4444/wd/hub',
     options=chrome_options
 )
